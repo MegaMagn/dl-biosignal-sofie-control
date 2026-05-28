2c7b4b0cd0f70c7679f5d9b33ac2b3a4fa8d3886
Magnus Malthe Sigsgaard Nielsen <134369832+MegaMagn@users.noreply.github.com>
2026-05-28 11:41:36 +0200

    Added Exoskeleton files

---
+++ Exoskeleton files/Code/Arduino_ESP32 project files/config.h
@@ -0,0 +1,10 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define WIFI_SSID "ProjektNet"
+#define WIFI_PASS "RobotRocks"
+#define PCAADDR 0x70
+#define PCA_CHANNEL 0
+#define MAG_THRESHOLD -0.25
+
+#endif
